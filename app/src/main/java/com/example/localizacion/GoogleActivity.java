dfcc4ec6e4e99b318ce2d498f520dd26f28ae9c7
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2022-03-30 10:51:09 -0500

    Light sensor done

--- app/src/main/java/com/example/localizacion/GoogleActivity.java
+++ app/src/main/java/com/example/localizacion/GoogleActivity.java
@@ -1,14 +1,27 @@
 package com.example.localizacion;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.localizacion.databinding.ActivityGoogleBinding;
 
@@ -17,9 +30,20 @@ public class GoogleActivity extends FragmentActivity implements OnMapReadyCallba
     private GoogleMap mMap;
     private ActivityGoogleBinding binding;
 
+    //Sensor de luz
+    SensorManager sensorManager;
+    Sensor lightSensor;
+    SensorEventListener lightEventListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Inflate
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        lightEventListener = createLightEventListener();
+
 
         binding = ActivityGoogleBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -28,6 +52,43 @@ public class GoogleActivity extends FragmentActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+    private SensorEventListener createLightEventListener(){
+        return new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(mMap!=null)
+                {
+                    if (sensorEvent.values[0] < 5000) {
+                        Log.i("MAPS", "DARK MAP " + sensorEvent.values[0]);
+                        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(GoogleActivity.this, R.raw.dark_map));
+                    } else {
+                        Log.i("MAPS", "LIGHT MAP " + sensorEvent.values[0]);
+                        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(GoogleActivity.this, R.raw.light_map));
+                    }
+                }
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(lightEventListener, lightSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(lightEventListener);
     }
 
     /**
@@ -43,9 +104,34 @@ public class GoogleActivity extends FragmentActivity implements OnMapReadyCallba
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //Configure gestures
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng puj = new LatLng(4.628553003284733, -74.06467244207825);
+        mMap.addMarker(new MarkerOptions().position(puj).title("Marker in PUJ")
+                .snippet("Edificio Ingeniería").icon(bitmapDescriptorFromVector(this, R.drawable.ic_baseline_location_on_24)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(puj));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+
+        //Personalizar mapa
+        mMap.setMapStyle(MapStyleOptions
+                .loadRawResourceStyle(this, 	R.raw.light_map));
+
+
+
+    }
+
+    public BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
