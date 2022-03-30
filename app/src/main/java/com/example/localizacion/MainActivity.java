1e2f09b6c7c3c9688513824817dfd177cad58ccc
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2022-03-30 09:58:34 -0500

    Google Maps added

--- app/src/main/java/com/example/localizacion/MainActivity.java
+++ app/src/main/java/com/example/localizacion/MainActivity.java
@@ -9,12 +9,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button simpleLocation;
+    Button google, osm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Inflate
         simpleLocation = findViewById(R.id.simpleLocation);
+        google = findViewById(R.id.google);
+        osm = findViewById(R.id.osm);
+
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), GoogleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
         simpleLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
